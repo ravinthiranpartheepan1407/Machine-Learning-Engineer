5e50b5e7c36f3837a72e3844aedccd0abd95ed35
surenravi1407 <surenravi1407@gmail.com>
2023-02-27 10:34:45 +0200

    Modules

---
+++ shopping-main-module.py
@@ -0,0 +1,2 @@
+import shopping.shoppinglistmodule
+print(shopping.shoppinglistmodule)
---
+++ shopping/shoppinglistmodule.py
@@ -0,0 +1,7 @@
+item = str(input("Enter item name: "))
+added_items = []
+def add_item(item):
+    added_items.append(item)
+    print("The added items are: ", added_items)
+
+add_item(item)
