e709f4e0ed4858a9ef9f7104ccaeec18d27c49f5
surenravi1407 <surenravi1407@gmail.com>
2023-03-09 10:52:53 +0200

    Match_Check + Analysis

--- data-structures/compare_list.py
+++ data-structures/compare_list.py
@@ -5,7 +5,7 @@
 
 ## Thought Process:
 
-# Bad Code:
+# Bad Code: ()
 # 1. Implementing for loop checking a match by iter thorugh data_1 and data_2 as seperate ops will create nested loop and results in O(n^2)
 
 # Good code:
@@ -20,13 +20,13 @@
 #3. Find a way to balance both rule 1 and rule 2
 
 data_1 = ['a', 'b', 'c', 'x'] #Time: O(1) #Space: O(1)
-data_2 = ['z','y','i'] #Time: O(1) #Space: O(1)
+data_2 = ['z','b','i'] #Time: O(1) #Space: O(1)
 
 match_1 = []
 match_2 = []
 
 data_3 = ['a', 'b', 'c', 'x'] #Time: O(1) #Space: O(1)
-data_4 = ['z','y','x'] #Time: O(1) #Space: O(1)
+data_4 = ['z','y','a'] #Time: O(1) #Space: O(1)
 
 def merge_1():
     for i in data_2: #Time: O(n) #Space: O(1)
@@ -35,12 +35,12 @@ def merge_1():
 
     for i in data_1: #Time: O(n) #Space: O(1)
         if data_1.count(i) > 1: #Time: O(1) #Space: O(1)
-            print("It has duplicates")
             match_1.append(i)
+            print("Match Found in data_1 and data_2: ", match_1)
             break
         else:
             match_1.append(None)
-            print("It has no duplicates")
+            print("No match Found")
 
 def merge_2():
     for i in data_4: #Time: O(n) #Space: O(1)
@@ -49,17 +49,16 @@ def merge_2():
 
     for i in data_3: #Time: O(n) #Space: O(1)
         if data_3.count(i) > 1: #Time: O(1) #Space: O(1)
-            print("It has duplicates", )
-            match_2.append(i) #Time: O(1) #Space: O(1
+            match_2.append(i) #Time: O(1) #Space: O(1)
+            print("Match Found in data_3 and data_4: ", match_2)
             break
         else:
             match_2.append(None) #Time: O(1) #Space: O(1
-            print("It has no duplicates")
+            print("No Match Found")
 
 merge_1()
-print("The matched letters are: ", match_1)
 merge_2()
-print("The matched letters are: ", match_2)
+
 
 print("The time complexity is: O(n+n) -> O(n)")
 
