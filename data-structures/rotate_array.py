97a8e065cb3169ac528c99026910e0cbd6485bc2
surenravi1407 <surenravi1407@gmail.com>
2023-03-17 10:31:44 +0200

    rotated_array

---
+++ data-structures/rotate_array.py
@@ -0,0 +1,26 @@
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+# Breakdown:
+
+# 1. Create a list with type of int
+# 2. Iterate through the loop with complexity of O(n)
+# 3. Set key value = userdefined (3)
+# 3. Get the last 3 items using [::-1] and push it to the rotated_list
+
+key = int(input("Enter a key to rotate: "))
+lists = [1,2,3,4,5,6,7]
+
+elements_storage_1  = []
+rotated_arr = []
+
+def rotated_array(key, lists): #O(n)
+    get_last = lists[:]
+    last_items = lists[:-key]
+    remove_duplicates = list(set(get_last) - set(last_items))
+    print(remove_duplicates)
+    print("The rotated array is: ", remove_duplicates+last_items)       
+    
+  
+rotated_array(key, lists)
+
+
